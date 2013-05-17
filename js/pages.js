c09e7ebfc5ee04ab0da66b6f2e1d30e6887d43c3
John Bissell <john@sunible.com>
2013-05-16 21:50:00 -0700

    commented out leaflet map attribution

--- js/pages.js
+++ js/pages.js
@@ -317,9 +317,9 @@ function showSocialProofMap(data)
 		$mapContainer.data('map', map);
 
 		L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png', {
-                    maxZoom: 18,
-                    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
-		}).addTo(map);
+                    maxZoom: 18
+//                  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
+                }).addTo(map);
 	}
 	var bounds = composeMapBounds(markerSource);
 	// adding markers to map

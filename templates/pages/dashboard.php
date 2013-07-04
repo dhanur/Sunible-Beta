4057a38672337d183a027ffd562d7f9a5afd411b
svlasenko <svt.vlasenko@gmail.com>
2013-07-04 17:18:38 +0300

    merge dashboard: providers -> installers

--- templates/pages/dashboard.php
+++ templates/pages/dashboard.php
@@ -7,9 +7,9 @@
 							You have selected<br/>
 							<span class="counter selected providers number" id="dashboard-block-number_of_providers_selected">0</span>
 							<br/>
-							installers
+							providers
 						</p>
-						<p class="max_length message">20 installers max</p>
+						<p class="max_length message">20 providers max</p>
 					</div>
 					<button type="button" 
                                         onMouseover="_gaq.push(['_trackEvent', 'Buttons', 'Mouseover', 'Dashboard Submit']);" 
@@ -18,7 +18,7 @@
 				</section>
 				<section class="providers container">
 					<header>
-						<h1>Select installers you’d like to get quotes from</h1>
+						<h1>Select providers you’d like to get quotes from</h1>
 						<p>(click columns to sort by Pricing, Homes Installed or Yelp Rating)</p>
 					</header>
 					<div class="table container"></div>

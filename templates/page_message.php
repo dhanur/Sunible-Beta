7b83cee18fa656a41bfc99ff84fee2f93494dfb4
John Bissell <john@sunible.com>
2013-07-03 17:31:34 -0700

    Merge branch 'origin/trunk'
    Conflicts:
    	contact_us.php
    	css/pages.css
    	index.php
    	jobs.php
    	templates/contact_us.php
    	templates/jobs.php
    	templates/pages/about_us.php
    	templates/pages/contact_us.php
    	templates/pages/dashboard.php
    	templates/pages/faq.php
    	templates/pages/home.php
    	templates/pages/jobs.php
    	templates/pages/modal_message.php
    	templates/pages/page_message.php
    	templates/pages/pp.php
    	templates/pages/registration.php
    	templates/pages/registration_thank_you.php
    	templates/pages/sign_up.php
    	templates/pages/social_proof.php
    	templates/pages/thank_you.php
    	templates/pages/tos.php

--- templates/page_message.php
+++
@@ -1,7 +0,0 @@
-<!-- PAGE MESSAGE -->
-<div
-	class="page message" id="page-message" data-page="message">
-	<!-- This page is dynamic.  Use it for messages like "Thank you", "Sorry", "Error", etc -->
-	<p class="loading message">loading...</p>
-</div>
-<!-- /PAGE MESSAGE -->

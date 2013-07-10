6f1a2d8e526443a57e98b5a6c499e14ffd4ccc5b
John Bissell <john@sunible.com>
2013-07-09 20:55:40 -0700

    Issue #18: call added
    getCountyFromZip(zip, callback) added to server-calls.js
    
    Signed-off-by: Roman Lytvynchuk <rlytvynchuk@gmail.com>

--- templates/pages/home.php
+++ templates/pages/home.php
@@ -1,22 +1,29 @@
-  			<!-- HOMEPAGE -->
-			<div class="page homepage show_header" id="page-homepage" data-page="homepage">
-				<section class="ad teaser container">
-					<a href="#" class="teaser" id="homepage_teaser_heading">
-						<span class="save">Save</span>
-						<span class="money_with">Money with</span>
-						<span class="solar">Solar</span>
-					</a>
-                                       
-					<form class="zip search providers container" id="homepage-search_providers_by_zip-container" >
-                                                <span class="field_container">
-							<input type="text" onFocus="_gaq.push(['_trackEvent', 'Form Fields', 'Focus', 'Landing Page Zip', 1, false]);" class="field zip" id="homepage-field-zip_code" name="zipcode" placeholder="Zip Code" pattern="\d{5}(?:[-\s]\d{4})?"/>
-							<span class="validation message"></span>
-						</span>
-						<button type="button" 
-                                                onMouseover="_gaq.push(['_trackEvent', 'Buttons', 'Mouseover', 'Landing Page Submit', 1, false]);" 
-                                                onClick="_gaq.push(['_trackEvent', 'Buttons', 'Click', 'Landing Page Submit', 1, false]);"
-                                                class="btn search providers zip" id="homepage-search_providers_by_zip-button">Go</button>
-					</form>
-				</section>			
-			</div>
-			<!-- /HOMEPAGE -->
+<!-- HOMEPAGE -->
+<div class="page homepage show_header" id="page-homepage"
+	data-page="homepage">
+	<section class="ad teaser container">
+		<a href="#" class="teaser" id="homepage_teaser_heading"> <span
+			class="money_with">3 Steps to Solar Savings with Sunible</span> 
+		</a>
+		<br/>
+				<p align="center">
+				<img border="0" src="http://sunible.com/assets/images/sunible.jpg" width="792" height="256">
+				</p>
+		<br/>
+		<form class="zip search providers container"
+			id="homepage-search_providers_by_zip-container" action="">
+			<span class="field_container"> <input type="text"
+				onFocus="_gaq.push(['_trackEvent', 'Form Fields', 'Focus', 'Landing Page Zip', 1, false]);"
+				class="field zip" id="homepage-field-zip_code" name="zipcode"
+				placeholder="Zip Code" pattern="\d{5}(?:[-\s]\d{4})?" autofocus /> <span
+				class="validation message"></span>
+			</span>
+			<button type="button"
+				onMouseover="_gaq.push(['_trackEvent', 'Buttons', 'Mouseover', 'Landing Page Submit', 1, false]);"
+				onClick="_gaq.push(['_trackEvent', 'Buttons', 'Click', 'Landing Page Submit', 1, false]);"
+				class="btn search providers zip"
+				id="homepage-search_providers_by_zip-button">Go</button>
+		</form>
+	</section>
+</div>
+<!-- /HOMEPAGE -->
